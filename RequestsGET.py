28d8e04a865985ac021d2988b9428935e1fdb5dc
Fructoze <143065276+ngththuyen@users.noreply.github.com>
2025-04-11 22:08:54 +0700

    Xóa các Option không cần thiết

--- RequestsGET.py
+++ RequestsGET.py
@@ -3,13 +3,9 @@ import requests
 url = f"" #Lấy link theo định dạng https://partyrock.aws/api/getApp?input={Encoded JSON App ID}
 
 headers = {
-    "Content-Type": "application/json",
     "Cookie": "",
     "User-Agent": "",
-    "Accept": "*/*",
-    "Accept-Encoding": "gzip, deflate, br, zstd",
     "Referer": "",
-    "Origin": "https://partyrock.aws",
     "Anti-Csrftoken-A2z":""
 }
 
